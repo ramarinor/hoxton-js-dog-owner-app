292719f2b7a1c4527a80d366139ee0a8fd96875a
Rinor Rama <rinor.rama95@gmail.com>
2021-11-24 17:45:10 +0100

    finished challenge

--- src/index.js
+++ src/index.js
@@ -120,38 +120,39 @@ function showForm() {
 	submitBtn.setAttribute("value", "Let's add a dog!");
 	submitBtn.setAttribute("class", "form__button");
 
+	//appending the created elements to the form and the form to the dog section
 	dogForm.append(nameLabel, nameInput, imageLabel, imageInput, bioLabel, bioTextarea, submitBtn);
 	dogSection.append(dogTitle, dogForm);
 
+	// adding submit event listener to add a new dog
 	dogForm.addEventListener("submit", function (event) {
 		event.preventDefault();
+		id++;
+		const newDog = {
+			id: id,
+			name: nameInput.value,
+			bio: bioTextarea.value,
+			isGoodDog: true,
+			image: imageInput.value,
+		};
+		data.unshift(newDog);
 		dogForm.reset();
+		//recreating the dog list
+		dogsList.innerHTML = `<li class="dogs-list__button dogs-list__button--add">+</li>`;
+		const addBtn = document.querySelector(".dogs-list__button--add");
+		addBtn.addEventListener("click", showForm);
+		updateDogCards(data);
 	});
+}
+function updateDogCards(data) {
+	for (const dog of data) {
+		updateDogCard(dog);
+	}
 }
 
 const dogsList = document.querySelector(".dogs-list");
 const dogSection = document.querySelector(".main__dog-section");
-
-for (const dog of data) {
-	updateDogCard(dog);
-}
-
+updateDogCards(data);
 const addBtn = document.querySelector(".dogs-list__button--add");
+let id = 7;
 addBtn.addEventListener("click", showForm);
-
-/* <section class="main__dog-section">
-<h2>Add a new Dog</h2>
-<form class="form">
-
-  <label for="name">Dog's name</label>
-  <input type="text" id="name" name="name">
-
-  <label for="image">Dog's picture</label>
-  <input type="url" id="image" name="image">
-
-  <label for="bio">Dog's bio</label>
-  <textarea rows="5" id="bio" name="bio"></textarea>
-
-  <input type="submit" id="submit" name="submit" value="Let's add a dog!" class="form__button">
-</form>
-</section> */
